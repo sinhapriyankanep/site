29fa297bbc2393210735db4d1171a0dc7a04ed8f
Priyanka_s2056 <87192868+sinhapriyankanep@users.noreply.github.com>
2024-06-23 09:57:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-/* document.addEventListener('DOMContentLoaded', () => {
-    const cursor = document.querySelector('.custom-cursor');
-    document.addEventListener('mousemove', (e) => {
-        cursor.style.left = `${e.clientX}px`;
-        cursor.style.top = `${e.clientY}px`;
-    });
-
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('mouseenter', () => {
-            cursor.style.transform = 'scale(1.5)';
-            cursor.style.backgroundColor = '#6a11cb';
-        });
-        link.addEventListener('mouseleave', () => {
-            cursor.style.transform = 'scale(1)';
-            cursor.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
-        });
-    });
-});
-document.addEventListener('DOMContentLoaded', function() {
-    var toggles = document.querySelectorAll('.toggle-input');
-    toggles.forEach(function(toggle) {
-        toggle.addEventListener('change', function() {
-            if (this.checked) {
-                closeAllToggles();
-                this.nextElementSibling.nextElementSibling.style.display = 'block';
-            } else {
-                this.nextElementSibling.nextElementSibling.style.display = 'none';
-            }
-        });
-    });
-
-    function closeAllToggles() {
-        toggles.forEach(function(toggle) {
-            toggle.nextElementSibling.nextElementSibling.style.display = 'none';
-            toggle.checked = false;
-        });
-    }
-});*/
 document.addEventListener('DOMContentLoaded', () => {
     const cursor = document.querySelector('.custom-cursor');
     document.addEventListener('mousemove', (e) => {
@@ -75,11 +37,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     const lastUpdatedElement = document.getElementById("lastUpdated");
     const currentDate = new Date();
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = currentDate.toLocaleDateString(undefined, options);
     lastUpdatedElement.textContent = formattedDate;
-});
+}); */
 
